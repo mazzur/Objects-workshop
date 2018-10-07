55387aac73d98490e21e50a8e0db09256442b978
mazzur <mazzur55@gmail.com>
2018-10-07 19:37:21 +0300

    Try to add new ball type with code duplication

--- bouncing-balls/index.js
+++ bouncing-balls/index.js
@@ -47,6 +47,42 @@ while (ballsPool.length < ballsCount) {
   ballsPool.push(ball);
 }
 
+const resizableBall = {
+  x: generateRandomNumber(0 + ballSize, canvasWidth - ballSize),
+  y: generateRandomNumber(0 + ballSize, canvasHeight - ballSize),
+  velocityX: generateRandomNumber(-maxVelocity, maxVelocity),
+  velocityY: generateRandomNumber(-maxVelocity, maxVelocity),
+  color: 'rgb(' + generateRandomNumber(0, 255) + ',' + generateRandomNumber(0, 255) + ',' + generateRandomNumber(0, 255) + ')',
+  size: ballSize,
+  draw() {
+    canvasContext.beginPath();
+    canvasContext.fillStyle = this.color;
+    canvasContext.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    canvasContext.fill();
+  },
+  update() {
+    const intersectVerticalBorder = (this.x + this.size) >= canvasWidth
+      || (this.x - this.size) <= 0;
+    const intersectHorizontalBorder = (this.y + this.size) >= canvasHeight
+      || (this.y - this.size) <= 0;
+
+    if (intersectVerticalBorder) {
+      this.velocityX = -(this.velocityX);
+    }
+
+    if (intersectHorizontalBorder) {
+      this.velocityY = -(this.velocityY);
+    }
+
+    this.x = this.x + this.velocityX;
+    this.y = this.y + this.velocityY;
+
+    this.size = generateRandomNumber(0, 25);
+  },
+};
+
+ballsPool.push(resizableBall);
+
 function moveBalls() {
   canvasContext.fillStyle = 'rgba(0,0,0,0.2)';
   canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
