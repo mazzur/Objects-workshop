84ed2aab1ceff9c923a94bdc7b1cdde14c61479e
mazzur <mazzur55@gmail.com>
2018-10-07 19:25:53 +0300

    Move the ball a bit and draw again

--- bouncing-balls/index.js
+++ bouncing-balls/index.js
@@ -15,10 +15,30 @@ const ball = {
     canvasContext.fillStyle = this.color;
     canvasContext.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     canvasContext.fill();
-  }
+  },
+  update() {
+    const intersectVerticalBorder = (this.x + this.size) >= canvasWidth
+      || (this.x - this.size) <= 0;
+    const intersectHorizontalBorder = (this.y + this.size) >= canvasHeight
+      || (this.y - this.size) <= 0;
+
+    if (intersectVerticalBorder) {
+      this.velocityX = -(this.velocityX);
+    }
+
+    if (intersectHorizontalBorder) {
+      this.velocityY = -(this.velocityY);
+    }
+
+    this.x = this.x + this.velocityX;
+    this.y = this.y + this.velocityY;
+  },
 };
 
 canvasContext.fillStyle = 'rgba(0,0,0,0.2)';
 canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
 
 ball.draw();
+ball.update();
+ball.draw();
+ball.update();
