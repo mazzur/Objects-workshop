92e8e3b180b76d9e4fff44c1655fcecf4b3fd17d
mazzur <mazzur55@gmail.com>
2018-10-21 22:41:54 +0300

    Improve the code a bit

--- bouncing-balls/index.js
+++ bouncing-balls/index.js
@@ -1,52 +1,64 @@
-const canvas = document.querySelector('canvas');
-const canvasContext = canvas.getContext('2d');
-const canvasWidth = canvas.width = window.innerWidth;
-const canvasHeight = canvas.height = window.innerHeight;
+var c;
+c = document
+  .querySelector('canvas');
+var ctx = c
+  .getContext(
+    '2d'
+  );
+var cW = c.width
+  = window
+  .innerWidth;
+var cH = c.height
+  = window
+  .innerHeight;
 
-function generateRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+function random(a, b) {
+  var random = Math.floor(Math.random()*(b-a))+a;
+  return random;
 }
 
-const ballsPool = [];
-const maxVelocity = 7;
-const ballsCount = 15;
+// balls pool
+var bp = new Array();
+// count
+var count = 15;
 
 class GenericBall {
   constructor() {
-    const ballSize = generateRandomNumber(10, 20);
-
-    this.x = generateRandomNumber(0 + ballSize, canvasWidth - ballSize);
-    this.y = generateRandomNumber(0 + ballSize, canvasHeight - ballSize);
-    this.velocityX = generateRandomNumber(-maxVelocity, maxVelocity);
-    this.velocityY = generateRandomNumber(-maxVelocity, maxVelocity);
-    this.color = 'rgb(' + generateRandomNumber(0, 255) + ',' + generateRandomNumber(0, 255) + ',' + generateRandomNumber(0, 255) + ')';
-    this.size = ballSize;
+  this.x = random(0
+    + random(10, 20),
+    cW - random(10, 20)
+  );
+  this.y = random(
+    0 + random(10, 20), cH - random(10, 20)
+  );
+  this.vH = random(-7, 7);
+  this.vY = random(-7, 7);
+  this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+  this.size = random(10, 20);
   }
+
+
   draw() {
-    canvasContext.beginPath();
-    canvasContext.fillStyle = this.color;
-    canvasContext.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    canvasContext.fill();
+    ctx.beginPath();
+
+    ctx.fillStyle = this.color;
+
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+
+    ctx.fill();
   }
   move() {
-    const intersectVerticalBorder = (this.x + this.size) >= canvasWidth
-      || (this.x - this.size) <= 0;
-    const intersectHorizontalBorder = (this.y + this.size) >= canvasHeight
-      || (this.y - this.size) <= 0;
-
-    if (intersectVerticalBorder) {
-      this.velocityX = -(this.velocityX);
-    }
-
-    if (intersectHorizontalBorder) {
-      this.velocityY = -(this.velocityY);
-    }
-
-    this.x = this.x + this.velocityX;
-    this.y = this.y + this.velocityY;
+    this.vH = ((this.x + this.size) >= cW || (this.x - this.size) <= 0) ? -(this.vH)
+      : this.vH;
+    this.vY = (this.y + this.size) >= cH
+|| (this.y - this.size) <= 0 ? -(this.vY) : this.vY;
+    this.x = this.x + this.vH;
+    this.y = this.y + this.vY;
   }
+
+
   update() {
-    this.move();
+    this.move()
   }
 }
 
@@ -55,29 +67,32 @@ class ResizableBall extends GenericBall {
     super();
   }
   resize() {
-    this.size = generateRandomNumber(0, 25);
+    this.size =
+      random(0, 25);
   }
   update() {
-    this.move();
-    this.resize();
+  this
+    .move();
+  this
+    .resize();
   }
 }
 
-while (ballsPool.length < ballsCount) {
-  const ball = new GenericBall();
-  ballsPool.push(ball);
+while (bp.length < count) {
+  ball = new GenericBall();
+  bp.push(ball);
 }
 
 const crazyBall = new ResizableBall();
-ballsPool.push(crazyBall);
+bp.push(crazyBall);
 
 function moveBalls() {
-  canvasContext.fillStyle = 'rgba(0,0,0,0.2)';
-  canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
+  ctx.fillStyle = 'rgba(0,0,0,0.2)';
+  ctx.fillRect(0, 0, cW, cH);
 
-  ballsPool.forEach((ball) => {
-    ball.draw();
-    ball.update();
+  bp.forEach((item) => {
+    item.draw();
+    item.update();
   });
 
   requestAnimationFrame(moveBalls);
